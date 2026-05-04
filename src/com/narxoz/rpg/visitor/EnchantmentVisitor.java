a94d3c69a753e36c543994e7c4f38bfe87a16357
nurbolatb0903-arch <nurbolat.b0903@icloud.com>
2026-05-04 20:38:56 +0500

    add EnchantmentVisitor for artifact enhancement

--- src/com/narxoz/rpg/visitor/EnchantmentVisitor.java
+++ src/com/narxoz/rpg/visitor/EnchantmentVisitor.java
@@ -1,4 +1,51 @@
 package com.narxoz.rpg.visitor;
 
-public class EnchantmentVisitor {
+import com.narxoz.rpg.artifact.*;
+
+public class EnchantmentVisitor implements ArtifactVisitor {
+    private int enchantmentCost = 0;
+
+    @Override
+    public void visit(Weapon weapon) {
+        int cost = weapon.getValue() / 2;
+        enchantmentCost += cost;
+        System.out.printf("  ⚔️ Enchanting %s: %d gold (Damage +%d)%n",
+                weapon.getName(), cost, weapon.getDamageBonus() / 2);
+    }
+
+    @Override
+    public void visit(Potion potion) {
+        int cost = potion.getValue() / 3;
+        enchantmentCost += cost;
+        System.out.printf("  🧪 Enhancing %s: %d gold (Heal +%d)%n",
+                potion.getName(), cost, potion.getHealAmount() / 2);
+    }
+
+    @Override
+    public void visit(Scroll scroll) {
+        int cost = scroll.getValue() / 2;
+        enchantmentCost += cost;
+        System.out.printf("  📜 Empowering %s: %d gold (Mana cost -%d)%n",
+                scroll.getName(), cost, scroll.getManaCost() / 3);
+    }
+
+    @Override
+    public void visit(Ring ring) {
+        int cost = ring.getValue() / 2;
+        enchantmentCost += cost;
+        System.out.printf("  💍 Infusing %s: %d gold (Mana Regen +%d, Defense +%d)%n",
+                ring.getName(), cost, ring.getManaRegen() / 2, ring.getDefenseBonus() / 2);
+    }
+
+    @Override
+    public void visit(Armor armor) {
+        int cost = armor.getValue() / 2;
+        enchantmentCost += cost;
+        System.out.printf("  🛡️ Reinforcing %s: %d gold (Defense +%d)%n",
+                armor.getName(), cost, armor.getDefenseRating() / 2);
+    }
+
+    public int getEnchantmentCost() {
+        return enchantmentCost;
+    }
 }
